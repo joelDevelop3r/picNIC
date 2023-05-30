970840786cfb31a65e98790c387459289fc98a25
re150 <a21110175@ceti.mx>
2023-05-30 12:41:19 -0600

    fin del diseño prototipo del Recycler ademas de agrgar las class MyAdaper

--- app/src/main/java/com/example/project/MainActivity.kt
+++ app/src/main/java/com/example/project/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var RecyclerView: RecyclerView
+    private lateinit var tareas :MutableList<String>
+    private lateinit var  layout : RecyclerView.LayoutManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tareas = getJob()// cree un funcion que simula los dato a resivir del json
+        RecyclerView= findViewById(R.id.recyclerView)
+        layout = LinearLayoutManager(this)
+
+    }
+
+    //Lista mutable con las tareas a realizar
+    private fun getJob(): MutableList<String>{
+        return  object  : ArrayList<String>(){
+            init{
+                add("Leer")
+                add("Jugar")
+                add("Escribir")
+                add("Escuchar musica")
+                add("Practicar")
+                add("Correr")
+                add("Cenar")
+                add("Ir al cine ")
+                add("Salir")
+                add("Gym")
+            }
+        }
     }
 }
