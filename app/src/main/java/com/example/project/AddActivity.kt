5c61591b77a2cf9e9683b4d1d6fec675eab6f708
kevinJoelLC <75746955+kevinJoelLC@users.noreply.github.com>
2023-06-03 12:44:44 -0600

    En esta rama se agrego la siguiente funcionalidad: 1- Se crea la clase 'Actividad' que describe cada tarea de la aplicacion
    2- se crea el objeto singleton en el archivo singleton.kt que almacena globalmente la lista de tareas
    
    3- Se crea el AddAtivity.kt que mostrara el formulario para crear una nueva actividad, cuando se guarda, regresa al main activity_main.xml
    
    4- Se crea el menu de opciones (se puede cambiar)
    
    5- Se cambia el nombre de la aplicacion a Agenda en strings.xml
    6- Se añade el icono de '+'
    
    7- Se añade a MainActivity.kt la funcion OnResume() que se ejecuta despues de regresar del formulario

---
+++ app/src/main/java/com/example/project/AddActivity.kt
@@ -0,0 +1,94 @@
+package com.example.project
+
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Gravity
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.*
+import java.util.*
+
+class AddActivity : AppCompatActivity() {
+
+
+    var añoActual=0
+    var mesActual=0
+    var díaActual=0
+    var horaActual=0
+    var minutoActual=0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        //Estas variables se utilizan para mostrar fecha y hora actual al abrir los time y date picker dialog
+        val calendar = Calendar.getInstance()
+        añoActual = calendar.get(Calendar.YEAR)
+        mesActual = calendar.get(Calendar.MONTH)
+        díaActual = calendar.get(Calendar.DAY_OF_MONTH)
+        horaActual = calendar.get(Calendar.HOUR_OF_DAY)
+        minutoActual = calendar.get(Calendar.MINUTE)
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add)
+
+
+
+
+
+        //Fecha y hora toman su valor aqui porque se usan dentro de buttonGuardar.setOnClickListener y de los pickers dialog
+        val foci: TextView = findViewById(R.id.editTextFecha)
+        val hora: TextView = findViewById(R.id.editTextHora)
+
+
+        val buttonGuardar: Button = findViewById(R.id.buttonGuardar)
+
+        buttonGuardar.setOnClickListener {
+            // Realizar acciones de guardado aquí
+            val nombre = findViewById<EditText>(R.id.editTextNombre).text.toString()
+            val descripcion = findViewById<EditText>(R.id.editTextDescripcion).text.toString()
+            val dificultad = when (findViewById<RadioGroup>(R.id.radioGroupDificultad).checkedRadioButtonId) {
+                R.id.radioAlta -> 1
+                R.id.radioMedia -> 2
+                R.id.radioBaja -> 3
+                else -> 0 // Valor por defecto en caso de que no se haya seleccionado ninguna opción
+            }
+            val favorita = findViewById<ToggleButton>(R.id.toggleFavorita).isChecked
+            val nuevaActividad = Actividad(nombre, descripcion, foci.text.toString(), hora.text.toString(), dificultad, favorita)
+
+            // Agregar la nuevaActividad a la lista del objeto singleton
+            singleton.agregarActividad(nuevaActividad)
+            //Termina actividad y la pantalla vuelve al MainActivity
+            finish()
+        }
+
+
+
+        // Funciones para inflar el date y time piker dialog y escribirlos en pantalla
+        val buttonSeleccionarFecha: Button = findViewById(R.id.buttonFecha)
+        val buttonSeleccionarHora: Button = findViewById(R.id.buttonHora)
+        buttonSeleccionarFecha.setOnClickListener {
+            val datePickerDialog = DatePickerDialog(this, { _, year, month, day ->
+                val fechaSeleccionada = "$day/${month + 1}/$year"
+                // Asignar la fecha seleccionada a un TextView o EditText en el formulario
+                foci.text = fechaSeleccionada
+            }, añoActual, mesActual, díaActual)
+            datePickerDialog.show()
+        }
+        buttonSeleccionarHora.setOnClickListener {
+            val timePickerDialog = TimePickerDialog(this, { _, hourOfDay, minute ->
+                val horaSeleccionada = String.format("%02d:%02d", hourOfDay, minute)
+                // Asignar la hora seleccionada a un TextView o EditText en el formulario
+                hora.setText(horaSeleccionada)
+            }, horaActual, minutoActual, true)
+            timePickerDialog.show()
+        }
+
+
+
+    }
+
+
+
+
+}
